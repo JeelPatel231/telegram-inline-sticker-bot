1393a7849144f20a9a089122083ce38b994aaa56
jeelpatel231 <jeelpatel231@gmail.com>
2022-03-29 23:18:01 +0530

    init push

---
+++ database.py
@@ -0,0 +1,116 @@
+from sqlalchemy import Table, Column, MetaData, String
+from sqlalchemy.dialects import postgresql
+from sqlalchemy import create_engine
+from sqlalchemy import insert, delete, select
+import os
+
+engine = create_engine(
+    os.environ["POSTGRES_URL"],
+    echo = True,
+)
+
+connection = engine.connect()
+
+metadata = MetaData()
+
+authorised = Table(
+    "authorised",
+    metadata,
+    Column('id',String,primary_key=True)
+)
+
+table = Table(
+    "stickers",
+    metadata,
+    Column('file_id',String,primary_key=True),
+    Column('file_unique_id',String),
+    Column('data', postgresql.ARRAY(String)),
+    Column('type',String)
+)
+
+metadata.create_all(engine)
+
+def add_authorised(id:str) -> None:
+    stmt = (
+        insert(authorised).
+        values(id=id)
+    )
+    connection.execute(stmt)
+
+def remove_authorised(id:str) -> None:
+    stmt = (
+        delete(authorised).
+        where(authorised.c.id==id)
+    )
+    connection.execute(stmt)
+
+def is_authorised(id:str) -> bool:
+    stmt = (
+        select(authorised).
+        where(authorised.c.id==id)
+    )
+    result = connection.execute(stmt)
+
+    try:
+        return result.fetchall()[0][0] == id
+    except IndexError:
+        return False
+
+def add_sticker(file_id:str,file_unique_id:str,tags:list,type:str) -> None:
+    stmt = (
+        insert(table).
+        values(file_id=file_id,file_unique_id=file_unique_id, data=tags, type=type)
+    )
+    connection.execute(stmt)
+
+def delete_sticker(file_unique_id:str) -> None:
+    stmt = (
+        delete(table).
+        where(table.c.file_unique_id==file_unique_id)
+    )
+    connection.execute(stmt)
+
+def get_tags(file_unique_id:str)->list:
+    stmt = (
+        select(table.c.data).
+        where(table.c.file_unique_id==file_unique_id)
+    )
+    return connection.execute(stmt).fetchall()
+    
+def add_tag(file_id:str,file_unique_id:str,new_tags:list,type:str) -> None:
+    # not using postgres array_append because it only
+    # supports single argument and not multiple
+
+    old_tags = get_tags(file_unique_id)
+
+    if not old_tags == []:
+        delete_sticker(file_id)
+    else:
+        add_sticker(file_id,file_unique_id,old_tags+new_tags,type)
+
+def remove_tag(file_id:str,file_unique_id,new_tags:list,type:str) -> None:
+    # not using postgres array_remove because it only
+    # supports single argument and not multiple
+
+    old_tags = get_tags(file_unique_id)
+
+    if not old_tags == []:
+        delete_sticker(file_id)
+        diff_list = [element for element in old_tags[0][0] if element not in new_tags]
+    else:
+        diff_list = new_tags
+    add_sticker(file_id,file_unique_id,diff_list,type)
+
+
+def fetch_sticker(tags:list)->list:
+    stmt = (
+        select(table.c.file_id,table.c.type).
+        where(table.c.data.contains(tags))
+    )
+    result = connection.execute(stmt)
+
+    temp_list = []
+    for i in result.fetchall():
+        temp_list.append(i)
+    
+    return temp_list
---
+++ main.py
@@ -0,0 +1,101 @@
+import telebot
+import os
+import database
+from telebot import types
+
+bot = telebot.TeleBot(os.environ["BOT_TOKEN"], parse_mode=None)
+owner_id=os.environ['OWNER_ID']
+
+method_map = {
+    "sticker" : types.InlineQueryResultCachedSticker,
+    "animation" : types.InlineQueryResultCachedGif
+}
+
+def is_owner(id:str) -> bool:
+    return id == owner_id
+
+def check_conditions_met(message,check_reply:bool = True,force_auth:bool=True,force_tags:bool = True) -> bool:
+    if check_reply and not message.reply_to_message:
+        bot.reply_to(message,"Reply to a Media/Message")
+        return False
+    
+    if force_auth and not (is_owner(str(message.from_user.id)) or database.is_authorised(str(message.from_user.id))):
+        bot.reply_to(message,"Not Authorised to contribute!")
+        return False
+
+    if force_tags and len(message.text.split(" ",1)) <= 1:
+        bot.reply_to(message,"no tags passed in message")
+        return False
+
+    return True
+
+def get_attrs(message,force_tags:bool = True) -> tuple:
+    type = message.reply_to_message.content_type
+    file_id = eval(f'message.reply_to_message.{type}.file_id')
+    file_unique_id = eval(f'message.reply_to_message.{type}.file_unique_id')
+    if not force_tags: return (file_id,file_unique_id,type)
+
+    tags = message.text.split(" ",1)[1].split(" ")
+    return (file_id,file_unique_id,tags,type)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Shut the fuck up!")
+
+@bot.message_handler(commands=['addtags'])
+def add_sticker_tags(message):
+    if not check_conditions_met(message): return
+    data = get_attrs(message)
+    database.add_tag(*data)
+    bot.reply_to(message,"added")
+
+@bot.message_handler(commands=['remtags'])
+def remove_sticker_tags(message):
+    if not check_conditions_met(message): return
+    data = get_attrs(message)
+    database.remove_tag(*data)
+    bot.reply_to(message,"removed")
+
+@bot.message_handler(commands=['delete'])
+def delete_sticker(message):
+    if not check_conditions_met(message,force_tags=False): return
+    data = get_attrs(message,False)
+    database.delete_sticker(data[1])
+    bot.reply_to(message,"deleted")
+
+@bot.message_handler(regexp="^/authori[sz]e$") # authorise , authorize
+def auth(message):
+    if not check_conditions_met(message,force_tags=False,force_auth=False): return
+    if not is_owner(str(message.from_user.id)):
+        bot.reply_to(message,"Only Owner can Auth/DeAuth peoples")
+        return
+    database.add_authorised(str(message.reply_to_message.from_user.id))
+    bot.reply_to(message,f"authorised {message.reply_to_message.from_user.username}")
+
+@bot.message_handler(regexp="^/[du][en]authori[sz]e$") # deauthori(s|z)e, unauthori(s|z)e
+def auth(message):
+    if not check_conditions_met(message,force_tags=False,force_auth=False): return
+    if not is_owner(str(message.from_user.id)):
+        bot.reply_to(message,"Only Owner can Auth/DeAuth peoples")
+        return
+    database.remove_authorised(str(message.reply_to_message.from_user.id))
+    bot.reply_to(message,f"deauthorised {message.reply_to_message.from_user.username}")
+
+@bot.message_handler(commands=["gettags","showtags"])
+def tags(message):
+    if not check_conditions_met(message,force_tags=False,force_auth=False): return
+    data = get_attrs(message,force_tags=False)
+    bot.reply_to(message,database.get_tags(data[1]))
+
+@bot.inline_handler(func=lambda m: True)
+def testmessage(inline_query):
+    results=[]
+    tags = inline_query.query.split(" ")
+    print(tags)
+    for index,response in enumerate(database.fetch_sticker(tags)):
+        results.append(method_map[response[1]](index,response[0]))
+        print(index,response)
+    bot.answer_inline_query(inline_query.id, results)
+
+
+bot.infinity_polling()
